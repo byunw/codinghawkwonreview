903621dca583d0fd03ace33f5a0218d1671e0b4f
Alex <wbyun1461@gmail.com>
2023-07-13 18:17:30 +0900

    first commit

---
+++ src/main/java/com/example/codinghawkwonreview/CodinghawkwonreviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.codinghawkwonreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodinghawkwonreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodinghawkwonreviewApplication.class, args);
+	}
+
+}
