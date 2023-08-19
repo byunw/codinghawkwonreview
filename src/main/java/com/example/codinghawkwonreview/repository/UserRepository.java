a88c37a6e30d0f84eeabab690396af8ee0277266
Alex <wbyun1461@gmail.com>
2023-08-19 17:08:56 +0900

    회원 서비스 레이어/리포지토리 레이어 반영

--- src/main/java/com/example/codinghawkwonreview/repository/UserRepository.java
+++ src/main/java/com/example/codinghawkwonreview/repository/UserRepository.java
@@ -1,2 +1,8 @@
-package com.example.codinghawkwonreview.repository;public interface UserRepository {
+package com.example.codinghawkwonreview.repository;
+
+import com.example.codinghawkwonreview.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User save(User user);
 }
--- src/main/java/com/example/codinghawkwonreview/service/UserService.java
+++ src/main/java/com/example/codinghawkwonreview/service/UserService.java
@@ -1,2 +1,22 @@
-package com.example.codinghawkwonreview.service;public class UserService {
+package com.example.codinghawkwonreview.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.codinghawkwonreview.repository.UserRepository;
+import com.example.codinghawkwonreview.domain.User;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public void register(User user){
+        userRepository.save(user);
+    }
+
 }
