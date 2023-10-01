1c9eb4c8148d78665428085f05ee9db2e0681c8e
Alex <wbyun1461@gmail.com>
2023-10-01 17:55:27 +0900

    컨트롤러 테스트 코드 추가

---
+++ src/test/java/com/example/codinghawkwonreview/controller/UserControllerTest.java
@@ -0,0 +1,36 @@
+package com.example.codinghawkwonreview.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import com.example.codinghawkwonreview.service.UserService;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = UserController.class)
+@WebAppConfiguration
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+    
+    @MockBean
+    private UserService userService;
+
+    @Test
+    public void 회원가입_컨트롤러_테스트() throws Exception{
+        mvc.perform(post("/userRegister")
+                .param("userEmail","wbyun1461@gmail.com")
+                .param("userPassword","1")
+                .param("userName","변우석"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("<h1>회원 가입에 성공 하셨습니다!</h1>"));
+    }
+
+}
